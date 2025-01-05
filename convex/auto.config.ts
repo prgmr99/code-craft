4ed8a8454e69e7558ac6cce958bd145a711c0bf0
prgmr99 <johnyeom24@naver.com>
2025-01-05 17:23:04 +0900

    chore: #11 fix auto config domain url

--- convex/auto.config.ts
+++ convex/auto.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://your-issuer-url.clerk.accounts.dev/",
+      domain: "https://adjusted-crab-19.clerk.accounts.dev/",
       applicationID: "convex",
     },
   ],
